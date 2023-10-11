86b81eb068834cc011ae61000f0746bad4efa28b
Kelly Siu <ksiu@LABA.PSWAS.LOCAL>
2023-10-11 16:07:48 -0700

    added app calls

--- 2ndExpress/express.js
+++ 2ndExpress/express.js
@@ -24,3 +24,23 @@ const movies = [ {
     director: "The Wachowskis", 
     year: 1999 
 } ];
+
+app.get("/", (req, res) => {
+    res.send(" Welcome to the Movie API! Use /info for guidance.")
+} )
+
+app.get("/info", (req,res) => {
+    res.send("To fetch all movies, use GET /movies. To add a new movie, use POST /movies. To update  or delete a movie, use PUT or DELETE on /movies/:id respectively.")
+})
+
+app.get("/movies", (req,res) => {
+    res.send(movies)
+})
+
+// app.get("/movies/:id", (res,req) => {
+//     //returns a specific movie based on the provided ID
+// })
+
+app.listen(3000, () => {
+    console.log("server has super started");
+})
